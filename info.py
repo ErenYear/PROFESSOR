2bc17802ca0d0597955950cabf415a1975f76caf
MYFLiiX bots <120738938+MYFLiiX@users.noreply.github.com>
2023-01-30 21:46:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -26,7 +26,7 @@ BOT_TOKEN = environ['BOT_TOKEN']
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
-PICS = (environ.get('PICS' ,'https://telegra.ph/file/2e2a07e86066538ed7406.jpg')).split()
+PICS = (environ.get('PICS' ,'https://te.legra.ph/file/6ff9762eea940a850f4d6.jpg')).split()
 BOT_START_TIME = time()
 
 # Admins, Channels & Users
